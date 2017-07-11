649a56466e4a93686e6e2a1bfd471b40163bf647
yangyongyi <yangyongyi_karen@hotmail.com>
2017-07-11 18:03:28 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express =  require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log+'\n', (err) => {
+        if(err){
+            console.log('Unable to append log message to logfile');
+        }
+    });
+    console.log(log);
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintanance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrenYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+    //response.send('<h1>Hello!!</h1>');
+    // response.send({
+    //     name: 'yyy',
+    //     likes: [
+    //         'code','cook'
+    //     ]
+    // });
+    response.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMsg: 'Welcome to yyy\'s website!'
+        //currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+        //currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'bad request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
